37a856ab409201f8b387ed0ed6ec985558651d38
AN <99376802+alainatividad@users.noreply.github.com>
2022-04-07 15:11:20 +1000

    fixed nav and used framework to load youtube videos

--- Assets/js/renderyoutube.js
+++ Assets/js/renderyoutube.js
@@ -2,8 +2,91 @@ $("document").ready(init);
 
 var searchKey = {};
 var videos = $();
-// var API_KEY = "AIzaSyB9Os5jr2rvfR85lNBk06y4o7wCamOMrgM";
-var API_KEY = "AIzaSyAkrMmNdavnKlZvW1vfVertGaylV7ucj2c";
+var videoContainer = $('#videos');
+var API_KEY = "AIzaSyB9Os5jr2rvfR85lNBk06y4o7wCamOMrgM";
+// var API_KEY = "AIzaSyAkrMmNdavnKlZvW1vfVertGaylV7ucj2c";
+// for testing
+// var data = {
+//   "kind": "youtube#searchListResponse",
+//   "etag": "J-s9sGIy5X8bniAIJSRfC4gRKQg",
+//   "nextPageToken": "CAIQAA",
+//   "regionCode": "AU",
+//   "pageInfo": {
+//     "totalResults": 1000000,
+//     "resultsPerPage": 2
+//   },
+//   "items": [
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "wbc6JE6HhAKC3PWaGEpGpO3zX6c",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "udKE1ksKWDE"
+//       },
+//       "snippet": {
+//         "publishedAt": "2017-02-18T16:06:43Z",
+//         "channelId": "UClVbhSLxwws-KSsPKz135bw",
+//         "title": "The Avengers - &quot;I&#39;m Always Angry&quot; - Hulk SMASH Scene - Movie CLIP HD",
+//         "description": "The Avengers (2012) - \"I'm Always Angry\" - Hulk SMASH Scene - Movie CLIP HD [1080p 60 FPS HD ] One of the best scene in ...",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/udKE1ksKWDE/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/udKE1ksKWDE/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/udKE1ksKWDE/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "TopMovieClips",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2017-02-18T16:06:43Z"
+//       }
+//     },
+//     {
+//       "kind": "youtube#searchResult",
+//       "etag": "tbagOUoBV4Xy5lcrfYu8aHb4dKo",
+//       "id": {
+//         "kind": "youtube#video",
+//         "videoId": "SLD9xzJ4oeU"
+//       },
+//       "snippet": {
+//         "publishedAt": "2017-01-22T21:08:03Z",
+//         "channelId": "UClVbhSLxwws-KSsPKz135bw",
+//         "title": "Thor vs Hulk - Fight Scene - The Avengers (2012) Movie Clip HD",
+//         "description": "Thor vs Hulk - Fight Scene - The Avengers (2012) Movie Clip 1080p HD All material belongs to Disney and Marvel. Fair use.",
+//         "thumbnails": {
+//           "default": {
+//             "url": "https://i.ytimg.com/vi/SLD9xzJ4oeU/default.jpg",
+//             "width": 120,
+//             "height": 90
+//           },
+//           "medium": {
+//             "url": "https://i.ytimg.com/vi/SLD9xzJ4oeU/mqdefault.jpg",
+//             "width": 320,
+//             "height": 180
+//           },
+//           "high": {
+//             "url": "https://i.ytimg.com/vi/SLD9xzJ4oeU/hqdefault.jpg",
+//             "width": 480,
+//             "height": 360
+//           }
+//         },
+//         "channelTitle": "TopMovieClips",
+//         "liveBroadcastContent": "none",
+//         "publishTime": "2017-01-22T21:08:03Z"
+//       }
+//     }
+//   ]
+// };
+
 
 function init() {
   try {
@@ -61,35 +144,34 @@ function videoSearch(key, search, maxResults) {
     console.log(data);
     displayVideos(data);
 
-    var tag = document.createElement("script");
-    // 2. This code loads the IFrameertBefo Player API code asynchronously.
-    $(tag).attr("src", "https://www.youtube.com/iframe_api");
-    var firstScriptTag = $("script")[6];
-    $("#videos").append(tag);
-    //  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+  //removed this as the videos are not responsive
+    // var tag = document.createElement("script");
+    // // 2. This code loads the IFrameertBefo Player API code asynchronously.
+    // $(tag).attr("src", "https://www.youtube.com/iframe_api");
+    // var firstScriptTag = $("script")[6];
+    // $("#videos").append(tag);
+    // //  firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
-    // from https://stackoverflow.com/questions/38906568/youtube-api-multiple-videos-with-events
-    var playerDivs = $(".player"); // get .player nodes
-    var playerDivsArr = [].slice.call(playerDivs); // nodelist to array to use forEach();
-    var players = new Array(playerDivsArr.length);
-    var waypoints = new Array(playerDivsArr.length);
+    // // from https://stackoverflow.com/questions/38906568/youtube-api-multiple-videos-with-events
+    // var playerDivs = $(".player"); // get .player nodes
+    // var playerDivsArr = [].slice.call(playerDivs); // nodelist to array to use forEach();
+    // var players = new Array(playerDivsArr.length);
+    // var waypoints = new Array(playerDivsArr.length);
 
-    // 3. This function creates an <iframe> (and YouTube player)
-    //    after the API code downloads.
-    // from https://chromatichq.com/insights/working-youtube-player-api-iframe-embeds
-    window.onYouTubeIframeAPIReady = function () {
-      playerDivsArr.forEach(function (e, i) {
-        // forEach ...
-        players[i] = new YT.Player(e.id, {
-          height: "360",
-          width: "640",
-          videoId: e.dataset.videoId,
-          events: {
-            /* no events set */
-          },
-        });
-      });
-    };
+    // // 3. This function creates an <iframe> (and YouTube player)
+    // //    after the API code downloads.
+    // // from https://chromatichq.com/insights/working-youtube-player-api-iframe-embeds
+    // window.onYouTubeIframeAPIReady = function () {
+    //   playerDivsArr.forEach(function (e, i) {
+    //     // forEach ...
+    //     players[i] = new YT.Player(e.id, {
+    //       videoId: e.dataset.videoId,
+    //       events: {
+    //         /* no events set */
+    //       },
+    //     });
+    //   });
+    // };
   });
 }
 
@@ -101,9 +183,20 @@ function displayVideos(data) {
     // 1. Create div for every iframe
     var player = $("<div>");
     $(player).attr("id", "player" + i);
-    $(player).addClass("player");
+    $(player).addClass("ui embed video player");
     $(player).attr("data-video-id", `${item.id.videoId}`);
+    $(player).attr("data-id", `${item.id.videoId}`);
+    $(player).attr("data-url", `https://www.youtube.com/embed/${item.id.videoId}`);
+    $(player).attr("data-source", 'youtube');
+    $(player).attr("data-image", `${item.snippet.thumbnails.high.url}`);
+    $(player).attr("data-placeholder", `${item.snippet.thumbnails.high.url}`);
+
+    var divider = $("<div>");
+    $(divider).addClass("ui hidden divider");
+    
     i++;
     $("#videos").append(player);
+    $("#videos").append(divider);
+    $('.ui.embed').embed();
   });
 }
